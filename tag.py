ad1a4541516f9a05d27c4c4b5966ee896983ceef
Brian Mock <mock.brian@gmail.com>
2011-05-10 20:16:41 -0700

    Added Tag class

---
+++ tag.py
@@ -0,0 +1,28 @@
+class Tag(object):
+    "A tag manages all the columns for a screen"
+
+    def __init__(self, **kwargs):
+        self.cur  = kwargs.get("cur",  None)
+        self.cols = kwargs.get("cols", [])
+
+    def move_win_col_num(self, number):
+        "Move the current window to the nth column"
+
+        win = self.take_cur_win()
+        if win is not None:
+            try:
+                tag = self.tags[number]
+                if tag is not None:
+                   tag.add_window(win)
+            except IndexError:
+                debug("Screen out of range")
+
+    def set_num_cols(self, number):
+        # TODO
+        debug("Tag.set_num_cols: PLEASE IMPLEMENT ME")
+
+    def select_col_num(self, number):
+        if between2(number, len(self.cols)):
+            self.cur = number
+        else:
+            debug("Column out of range")
