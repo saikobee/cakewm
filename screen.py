bc318bb5da9e7ad0edf39b71d5addbc278f83d4b
Brian Mock <mock.brian@gmail.com>
2011-05-10 19:04:04 -0700

    Added a Screen class

---
+++ screen.py
@@ -0,0 +1,21 @@
+class Screen(object):
+    "A screen manages the set of tags associated with a single screen"
+
+    def __init__(self, **kwargs):
+        self.cur  = kwargs.get("cur",  None)
+        self.tags = kwargs.get("tags", [])
+
+    def move_win_tag(self, number):
+        "Moves the current window to the tag identified by the given number"
+
+        if self.cur is not None:
+            try:
+                win = self.take_cur_win()
+                if win is not None:
+                    self.tags[self.cur].add_window(win)
+            except IndexError:
+                debug("Attempted to move window to non-existent tag")
+
+    def select_tag(self, number):
+        if between2(number, len(self.tags)):
+            self.cur = number
