7ecb257c471f17ceb76b81e3841f74c520aeae1e
Brian Mock <mock.brian@gmail.com>
2011-05-22 16:02:20 -0700

    Fixed unused row/column of pixels bug

--- cake/wm.py
+++ cake/wm.py
@@ -4,9 +4,17 @@ class WM(object):
 
     def organize(self):
         for i_screen, screen in enumerate(self.display.screens):
+            n_screens = len(self.display.screens)
+
             for i_tag, tag in enumerate(screen.tags):
+                n_tags = len(screen.tags)
+
                 for i_col, col in enumerate(tag.cols):
+                    n_cols = len(tag.cols)
+
                     for i_stack, stack in enumerate(col.stacks):
+                        n_stacks = len(col.stacks)
+
                         stack.unfocus()
 
                         indices = (
@@ -26,10 +34,22 @@ class WM(object):
                         if indices == curs:
                             stack.focus()
 
-                        stack.w = screen.w / len(tag.cols)
-                        stack.h = screen.h / len(col.stacks)
+                        stack.w = screen.w / n_cols
+                        stack.h = screen.h / n_stacks
                         stack.x = stack.w * i_col
                         stack.y = stack.h * i_stack
+
+                        # We might not use every pixel on the screen
+                        # given this approach, so fix the things near
+                        # screen edges.
+
+                        # Right edge
+                        if i_col == n_cols - 1:
+                            stack.w = screen.w - (int(screen.w / n_cols) * (n_cols - 1))
+
+                        # Bottom edge
+                        if i_stack == n_stacks - 1:
+                            stack.h = screen.h - (int(screen.h / n_stacks) * (n_stacks - 1))
 
                         stack.organize()
 
