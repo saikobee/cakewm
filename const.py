022e6d6999166770b03331968b63595cc71f8096
Brian Mock <mock.brian@gmail.com>
2011-05-23 08:34:04 -0700

    Added __cmp__ for Const

--- const.py
+++ const.py
@@ -2,6 +2,9 @@ class Const(object):
     def __init__(self, num):
         self.num = num
 
+    def __cmp__(self, that):
+        return self.num - that.num
+
 NEXT = Const(+1)
 CUR  = Const( 0)
 PREV = Const(-1)
