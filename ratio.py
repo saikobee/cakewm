7fb80637dacbef050d63ec62258afc3cb4e858cf
Brian Mock <mock.brian@gmail.com>
2011-05-22 17:57:49 -0700

    Lower ratio min. Max now defined in terms of min.

--- ratio.py
+++ ratio.py
@@ -1,8 +1,8 @@
 class Ratio(object):
     "Ratio merely has one member: `ratio', with a default value: 0.50"
     DEFAULT = 0.50
-    MIN     = 0.10
-    MAX     = 0.90
+    MIN     = 0.05
+    MAX     = 1 - MIN
 
     def __init__(self, ratio=DEFAULT):
         self.ratio = ratio
