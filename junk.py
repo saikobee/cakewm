1cdb80582dfb2aadb819879b53912b222fd2075e
Brian Mock <mock.brian@gmail.com>
2011-05-29 00:47:59 -0600

    Aded more screens and tags

--- junk.py
+++ junk.py
@@ -14,7 +14,7 @@ def mk(klass, func, num):
 mk_stack    = mk(Stack,     Window,     1)
 mk_col      = mk(Column,    mk_stack,   2)
 mk_tag      = mk(Tag,       mk_col,     2)
-mk_screen   = mk(Screen,    mk_tag,     2)
-mk_display  = mk(Display,   mk_screen,  1)
+mk_screen   = mk(Screen,    mk_tag,     4)
+mk_display  = mk(Display,   mk_screen,  2)
 
 display = mk_display()
