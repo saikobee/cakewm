abd7bb9d0408f2a34ec3b8d9667d813e5530d68c
Brian Mock <mock.brian@gmail.com>
2011-06-23 20:49:46 -0600

    Shortened a variable name

--- ratio.py
+++ ratio.py
@@ -2,13 +2,13 @@ import util
 
 class Ratio(object):
     "Ratio merely has one member: `ratio', with a default value: 0.50"
-    DEFAULT = 0.50
-    MIN     = 0.05
-    MAX     = 1 - MIN
-    INC     = 0.05
-    DEC     = -INC
+    DEF = 0.50
+    MIN = 0.05
+    MAX = 1 - MIN
+    INC = 0.05
+    DEC = -INC
 
-    def __init__(self, ratio=DEFAULT):
+    def __init__(self, ratio=DEF):
         self.ratio = ratio
 
     def complement_ratio(self): self.ratio = 1 - self.ratio
